621783238f46f7c0519022c211e7637b9919d30a
Richard <richard_madrigar99@hotmail.com>
2025-09-27 04:03:55 -0300

    add capture utm

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,25 @@ import { Box, CssBaseline, ThemeProvider } from '@mui/material';
 import { Analytics } from '@vercel/analytics/react';
 import theme from './theme';
 import LandingPage from './pages/LandingPage';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 export function App() {
+  useEffect(() => {
+    const url = window.location.href
+
+    const sendView = async () => {
+      try {
+        await axios.post('https://function-bun-production-079a.up.railway.app/createView', { url })
+        console.log('View registrada:', url)
+      } catch (err) {
+        console.error('Erro ao registrar view:', err)
+      }
+    }
+
+    sendView()
+  }, []) // executa só uma vez ao montar
+
   return (
     <ThemeProvider theme={theme}>
       <Box sx={{ minHeight: '100vh' }}>
